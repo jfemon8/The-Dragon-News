b1979d72b60f6c6f18728fe668bb593ceb1511d1
jfemon8 <jfemon8@gmail.com>
2025-05-11 19:56:42 +0600

    Set up router

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hi, I'm Emon</h1>,
+  },
+]);
+
+export default router;
